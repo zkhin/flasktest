137d25a6a40e4f8614050a8b7649786d1a77e63a
zayar <zkhin@github.com>
2018-10-06 14:40:56 -0400

    1

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Zayar Khin',
+        'title': 'Blog Post1',
+        'content': 'First post content',
+        'date_posted': 'Nov 6, 2018'
+    }
+]
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
-
+    return render_template('about.html')
 if __name__ == '__main__':
     app.run(debug=True)
